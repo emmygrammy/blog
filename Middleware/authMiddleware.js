06c656a73a2036fe3e71505a69b36e82e086e669
emmy grammy <ojeabuo24@gmail.com>
2026-04-30 01:10:08 +0100

    learnlift-backend

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -2,10 +2,9 @@ import jwt from "jsonwebtoken";
 import Admin from "../Models/AdminModel.js";
 
 export const protectAdmin = async (req, res, next) => {
-  let token;
-
   try {
-    // 1. Get token from header
+    let token;
+
     if (
       req.headers.authorization &&
       req.headers.authorization.startsWith("Bearer")
@@ -13,24 +12,20 @@ export const protectAdmin = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
     }
 
-    // 2. If no token
     if (!token) {
       return res.status(401).json({ message: "Not authorized, no token" });
     }
 
-    // 3. Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // 4. Attach admin to request
-    req.admin = await Admin.findById(decoded.id).select("-password");
+    req.admin = await Admin.findById(decoded.admin).select("-password");
 
     if (!req.admin) {
       return res.status(401).json({ message: "Admin not found" });
     }
 
-    // 5. Move to next middleware/controller
     next();
-    
+
   } catch (error) {
     return res.status(401).json({ message: "Not authorized, token failed" });
   }
